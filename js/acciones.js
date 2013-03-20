3d9aeffed974f821d47ab9d1fa3a67a2cadf393d
Luis Joel Valencia Rojas <luissjoell@gmail.com>
2013-03-20 08:00:19 -0600

    Version 1.0

---
+++ js/acciones.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    alert ("El documento esta lsito")
+
+document.addEventListener("diviceready", function(){
+	$('.btn').click(function(){
+var boton=(($(this).attr('class')).split (' '))[1];
+	switch (boton)
+	{
+		case 'b1':
+			navigator.notification.beep(1);
+		break;
+		case 'b2':
+			navigator.notification.beep(2);
+		break;
+		case 'b3':
+			navigator.notification.beep(5);
+		break;
+		case 'b4':
+			navigator.notification.beep(10);
+		break;
+		case 'v1':
+			navigator.notification.vibrate(500);
+		break;
+		case 'v2':
+			navigator.notification.vibrate(500);
+			navigator.notification.vibrate(500);
+		break;
+	}
+	
+	});
+	$('#derecha').swiperight(function(){
+	navigator.notification.alert("Deliz&oacute; a la derecha",function() {},"practica1","aceptar");
+	});
+},false);
+});
